aef7d76ad3e7fba24b210d3f8164b87b38d07109
Romikboom <abesadzeromiko23@gmail.com>
2024-11-28 23:21:50 +0400

    first commit

---
+++ level 03/classwork/cw.py
@@ -0,0 +1,25 @@
+
+name = "დავით"
+last_name = "გოგლიძე"
+
+
+full_name = name + " " + last_name
+
+
+print("მხოლოდ სახელი და გვარი:", full_name)
+
+
+num1 = 5  
+num2 = 3.14  
+
+
+sum_result = num1 + num2  
+diff_result = num1 - num2  
+prod_result = num1 * num2  
+quot_result = num1 / num2  
+
+
+print(f"ჯამი: {sum_result}")
+print(f"სხვაობა: {diff_result}")
+print(f"ნამრავლი: {prod_result}")
+print(f"გამყოფი: {quot_result}")
